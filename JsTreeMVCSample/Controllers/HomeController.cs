02e5ac2c3a64181673d1dac3dbb1d48a4e78e81d
Rajasekhar Naidu <rajasekhard2015@outlook.com>
2021-09-06 09:48:32 -0700

    js tree sample

---
+++ JsTreeMVCSample/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using JsTreeMVCSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsTreeMVCSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public ActionResult Nodes()
+        {
+            var nodes = new List<JsTreeModel>();
+            nodes.Add(new JsTreeModel() { id = "101", parent = "#", text = "Simple root node" });
+            nodes.Add(new JsTreeModel() { id = "102", parent = "#", text = "Root node 1" });
+            nodes.Add(new JsTreeModel() { id = "103", parent = "#", text = "Child 1" });
+            nodes.Add(new JsTreeModel() { id = "104", parent = "#", text = "Child 2" });
+            return Json(nodes);
+        }
+    }
+}
---
+++ JsTreeMVCSample/Models/JsTreeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsTreeMVCSample.Models
+{
+    public class JsTreeModel
+    {
+        public string id { get; set; }
+        public string parent { get; set; }
+        public string text { get; set; }
+        public string icon { get; set; }
+        public string state { get; set; }
+        public bool opened { get; set; }
+        public bool disabled { get; set; }
+        public bool selected { get; set; }
+        public string li_attr { get; set; }
+        public string a_attr { get; set; }
+    }
+}
